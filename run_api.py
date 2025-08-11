d5a4511253b7493792fe75222abd79aad64d290e
Xenrose <k_penrose@naver.com>
2025-08-12 00:02:56 +0900

    update

--- run_api.py
+++ run_api.py
@@ -1,17 +1,12 @@
-from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse, JSONResponse
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from fastapi.responses import HTMLResponse
-from pathlib import Path
-from io import BytesIO
-import base64
 
 from router import (
-    inspect,
     load,
-    register
+    inspect,
+    save,
+    extract
     )
 
 
@@ -19,16 +14,9 @@ from router import (
 app = FastAPI()
 
 app.include_router(inspect.router, prefix="/api/v1")
-app.include_router(register.router, prefix="/api/v1")
 app.include_router(load.router, prefix="/api/v1")
-# app.include_router(ip_check.router, prefix="/api/v1")
-# app.include_router(summary_objects.router, prefix="/api/v1")
-# app.include_router(json_schema_extract.router, prefix="/api/v1")
-# app.include_router(json_validate.router, prefix="/api/v1")
-
-
-
-
+app.include_router(save.router, prefix="/api/v1")
+app.include_router(extract.router, prefix="/api/v1")
 
 
 
@@ -39,34 +27,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-# def init_data():
-#     data = []
-#     target_path = Path(__file__).parent / "test_input"
-
-#     html_png_list = sorted(list((target_path / "html_to_png").rglob("*.png")))
-#     html_list = sorted(list((target_path / "html").rglob("*.html")))
-#     raw_png_list = sorted(list((target_path / "png").rglob("*.png")))
-
-#     for html_png, html, raw_png in zip(html_png_list, html_list, raw_png_list):
-#         text_info = html_png.stem
-#         html_png_base64 = img_to_base64(html_png)
-#         raw_png_base64 = img_to_base64(raw_png)
-#         html_base64 = read_html_file(html)
-
-#         data.append({
-#             "mainImage": "data:image/png;base64," +raw_png_base64,
-#             "hoverImage": "data:image/png;base64," + html_png_base64,
-#             "html": html_base64,
-#             "hiddenTextInfo": text_info
-#         })
-#     return data
-        
-        
-
-# data = init_data()
-
 
 
 
